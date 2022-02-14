783bd5a33adea5b998fb6236c2ea661ca0883ea7
Dustin Carlino <dabreegster@gmail.com>
2022-02-14 10:58:36 +0000

    Revert "Get the Rust snapshot to at least load"
    This reverts commit ddddf3a8d316793852393b6925b28321681b04fc.
    
    Going to properly fix this now

--- coding/initialise/initialisation_cache.py
+++ coding/initialise/initialisation_cache.py
@@ -25,18 +25,15 @@ class InitialisationCache:
             json.dump(shpfile, output_file)
 
     def read_from_cache(self):
-        #individuals = pd.read_pickle(self.individuals_filepath)
-        individuals = None
-        #with open(self.activity_locations_filepath, 'rb') as handle:
-        #    activity_locations = pickle.load(handle)
-        activity_locations = None
+        individuals = pd.read_pickle(self.individuals_filepath)
+        with open(self.activity_locations_filepath, 'rb') as handle:
+            activity_locations = pickle.load(handle)
         lockdown = pd.read_csv(self.lockdown_filepath)
         return individuals, activity_locations, lockdown
 
     def cache_files_exist(self):
-        #files_exist = [os.path.exists(cache_file) for cache_file in self.all_cache_filepaths]
-        #return all(files_exist)
-        return True
+        files_exist = [os.path.exists(cache_file) for cache_file in self.all_cache_filepaths]
+        return all(files_exist)
 
     def is_empty(self):
         return not self.cache_files_exist()
